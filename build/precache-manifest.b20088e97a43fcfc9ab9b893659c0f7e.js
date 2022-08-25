4d461bf33a644a797c7596b54040bca84f1b3689
Manikarthik Chandankeri <mc21407@corp.ojas-it.com>
2022-08-25 09:43:12 +0530

    react_Frontend

---
+++ build/precache-manifest.b20088e97a43fcfc9ab9b893659c0f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5e79fff2bf5d771ecaae00de6106a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42ae131b0adda8ac568",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "ad3368fcf8c9924a632e",
+    "url": "/static/js/2.c6a70f71.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6a70f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42ae131b0adda8ac568",
+    "url": "/static/js/main.5a319a5b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
